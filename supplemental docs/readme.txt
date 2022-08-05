8dc6de779072b25380eb8f24fe70762667e70d43
Nick Painter <nickcpainter@gmail.com>
2022-08-05 17:54:53 -0500

    feat: ability to just add GPU-PV without parsec by utilizing `withoutParsec.ps1` and `Update-VMGpuPartitionDriver.ps1`. docs: supplemental info

---
+++ supplemental docs/readme.txt
@@ -0,0 +1,3 @@
+In order run `Update-VMGpuPartitionDriver` mount the vhd and update the value of `$DriveLetter = "I"` in the script to the letter of the mounted vhd.
+
+https://www.tenforums.com/virtualization/195745-tutorial-passing-through-gpu-hyper-v-guest-vm.html
---
+++ withoutParsec.ps1
@@ -0,0 +1,8 @@
+$vm = "Danfoss2"
+if (Get-VMGpuPartitionAdapter -VMName $vm -ErrorAction SilentlyContinue) {
+   Remove-VMGpuPartitionAdapter -VMName $vm
+}
+Set-VM -GuestControlledCacheTypes $true -VMName $vm
+Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $vm
+Set-VM -HighMemoryMappedIoSpace 32Gb -VMName $vm
+Add-VMGpuPartitionAdapter -VMName $vm
